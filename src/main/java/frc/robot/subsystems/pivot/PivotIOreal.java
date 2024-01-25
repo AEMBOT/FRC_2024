1ee0f2b07789c3ad96e372aea451a706d33f463c
Zoe <zoe.murray011@gmail.com>
2024-01-24 19:25:55 -0800

    Written pivot needs

---
+++ src/main/java/frc/robot/subsystems/pivot/PivotIOreal.java
@@ -0,0 +1,6 @@
+//has to rotate up and down
+//stay in fixed angle
+//uses 2 neo motors (opposite sides) with sparkmax controlers
+//Absolute encoder
+//set current limit (low)
+//flows w/controler, press button -> go to fixed angle 
