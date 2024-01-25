678f8c12407ad36d1cc162613b1ae02d7508e75f
TheFlameFish <mccoa499@gmail.com>
2024-01-25 10:02:55 -0800

    Created (currently empty) Intake class

--- src/main/java/frc/robot/subsystems/Indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer/Indexer.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.Indexer;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
---
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.Intake;
+
+public class Intake {
+
+}
