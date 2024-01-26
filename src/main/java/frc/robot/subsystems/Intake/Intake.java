40fccbf5e6631b09bf57d02eb972249505fbe672
chase <chase.e.stafford@gmail.com>
2024-01-25 16:08:44 -0800

    setup basic intake

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,3 +1,24 @@
 package frc.robot.subsystems.Intake;
 
-public class Intake {}
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase {
+  private final IntakeIO io;
+
+  public Intake(IntakeIO io) {
+    this.io = io;
+  }
+
+  public void IndexerIn() {
+    io.SetVoltage(Constants.IndexerConstants.indexerMotorVoltage);
+  }
+
+  public void IndexerOut() {
+    io.SetVoltage(-Constants.IndexerConstants.indexerMotorVoltage);
+  }
+
+  public void IndexerStop() {
+    io.SetVoltage(0);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIO.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.Intake;
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IntakeIO {
+
+  @AutoLog
+  public static class IntakeIOinputs {
+    public double appliedVolts = 0.0;
+  }
+
+  public default void SetVoltage(double volts) {}
+}
+
