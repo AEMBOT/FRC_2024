f61e847e61e723b8a1d2d8b39ebd96033f5bfdb2
Wither <86643519+ItsDaWither@users.noreply.github.com>
2024-01-29 15:12:32 -0800

    drivebase constant updates

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -42,9 +42,9 @@ import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
 public class Drive extends SubsystemBase {
-  private static final double MAX_LINEAR_SPEED = Units.feetToMeters(14.5);
-  private static final double TRACK_WIDTH_X = Units.inchesToMeters(25.0);
-  private static final double TRACK_WIDTH_Y = Units.inchesToMeters(25.0);
+  private static final double MAX_LINEAR_SPEED = Units.feetToMeters(17.14); // MK4i L2+
+  private static final double TRACK_WIDTH_X = Units.inchesToMeters(22.75); // 28 in square chassis
+  private static final double TRACK_WIDTH_Y = Units.inchesToMeters(22.75);
   private static final double DRIVE_BASE_RADIUS =
       Math.hypot(TRACK_WIDTH_X / 2.0, TRACK_WIDTH_Y / 2.0);
   private static final double MAX_ANGULAR_SPEED = MAX_LINEAR_SPEED / DRIVE_BASE_RADIUS;
