9f842355145c41c2742ccef82728403612ef3945
TheFlameFish <mccoa499@gmail.com>
2024-01-29 17:13:54 -0800

    Added commands for indexer/intake motor control

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.indexer;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Indexer extends SubsystemBase {
@@ -10,27 +11,53 @@ public class Indexer extends SubsystemBase {
     this.io = io;
   }
 
-  public void IndexerIn() {
+  public void indexerIn() {
     io.setShooterIndexer(IndexerIO.IndexerIOInputs.MotorState.IN);
   }
 
-  public void IndexerOut() {
+  public void indexerOut() {
     io.setShooterIndexer(IndexerIO.IndexerIOInputs.MotorState.OUT);
   }
 
-  public void IndexerStop() {
+  public void indexerStop() {
     io.setShooterIndexer(IndexerIO.IndexerIOInputs.MotorState.OFF);
   }
 
-  public void IntakeIn() {
+  public void intakeIn() {
     io.setIntakeIndexer(IndexerIO.IndexerIOInputs.MotorState.IN);
   }
 
-  public void IntakeOut() {
+  public void intakeOut() {
     io.setIntakeIndexer(IndexerIO.IndexerIOInputs.MotorState.OUT);
   }
 
-  public void IntakeStop() {
+  public void intakeStop() {
     io.setIntakeIndexer(IndexerIO.IndexerIOInputs.MotorState.OFF);
+  }
+
+  // Commands
+
+  public Command intakeInCommand() {
+    return runOnce(this::intakeIn);
+  }
+
+  public Command intakeOutCommand() {
+    return runOnce(this::intakeOut);
+  }
+
+  public Command intakeStopCommand() {
+    return runOnce(this::intakeStop);
+  }
+
+  public Command indexerInCommand() {
+    return runOnce(this::indexerIn);
+  }
+
+  public Command indexerOutCommand() {
+    return runOnce(this::indexerOut);
+  }
+
+  public Command indexerStopCommand() {
+    return runOnce(this::indexerStop);
   }
 }
