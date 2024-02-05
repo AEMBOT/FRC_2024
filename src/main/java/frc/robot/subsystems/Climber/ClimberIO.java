ef339837d9c83e0e2fbe93867e12af5d5b0a6339
Holden <Ashlythetall@gmail.com>
2024-02-05 10:16:19 -0800

    Forgot to commit. Added some controls to the climber area.

--- src/main/java/frc/robot/subsystems/Climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberIO.java
@@ -1,5 +1,31 @@
 package frc.robot.subsystems.Climber;
 
-public class ClimberIO {
+import org.littletonrobotics.junction.AutoLog;
+
+import frc.robot.subsystems.pivot.PivotIO.PivotIOInputs;
+
+public interface ClimberIO {
+
     
+} ClimberIO {
+    @AutoLog
+    public static class climberIOInputs {
+        public double armPosition = 0.0;
+        public double appliedVolts = 0.0;
+        public double[] currentAmps = new double[] {};
+    }
+    /** Updates the set of loggable inputs. */
+  public default void updateInputs(climberIOInputs inputs) {}
+
+  /** Sets the hight of the arm, in radians. */
+  public default void setPosition(double armPositionPosition, double ffVolts) {}
+
+  /** Run open loop at the specified voltage. */
+  public default void setVoltage(double volts) {}
+
+  /** Stop in open loop. */
+  public default void stop() {}
+
+  /** Set position PID constants. */
+  public default void configurePID(double kP, double kI, double kD) {}
 }
