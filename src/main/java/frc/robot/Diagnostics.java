495863a004a9a4b25e900dfff8421dd67596e087
Ace5tar <tace233@gmail.com>
2024-02-08 18:20:33 -0800

    drivebase test command & pivot test command

--- src/main/java/frc/robot/Diagnostics.java
+++ src/main/java/frc/robot/Diagnostics.java
@@ -1,5 +1,34 @@
 package frc.robot;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.pivot.Pivot;
+
 public class Diagnostics {
+
+  private Diagnostics() {}
+
+  // run swerve motors
+
+  public static Command testDrivetrainCommand(Drive drive) {
+    return drive.runVelocityCommand(() -> new ChassisSpeeds(1, 1, 1));
+  }
+
+  // run shooter motors
+
+  // run pivot motors
+
+  public static Command testPivotMaxAngleCommand(Pivot pivot) {
+    return pivot.goToAngle(Constants.PivotConstants.PIVOT_MAX_POS_RAD);
+  }
+
+  public static Command testPivotMinAngleCommand(Pivot pivot) {
+    return pivot.goToAngle(Constants.PivotConstants.PIVOT_MIN_POS_RAD);
+  }
+
+  // run indexer motors
+
+  // run climber motors
 
 }
