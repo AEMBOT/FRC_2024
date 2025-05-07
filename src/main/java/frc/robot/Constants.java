b7e7e1720ad1fbc14ebd6ec93d43658226390623
Ace5tar <tace233@gmail.com>
2025-05-06 18:06:55 -0700

    changed lightcycle to clef

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,7 +32,7 @@ import edu.wpi.first.wpilibj.RobotBase;
 public final class Constants {
   public static final DigitalInput robotJumper = new DigitalInput(0);
   public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : Mode.REPLAY;
-  public static final Robot currentRobot = robotJumper.get() ? Robot.CLEF : Robot.LIGHTCYCLE;
+  public static final Robot currentRobot = robotJumper.get() ? Robot.LIGHTCYCLE : Robot.CLEF;
 
   public enum Mode {
     /** Running on a real robot. */
